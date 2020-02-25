03e3fad7b7acfc79d58fe4e9ea0e3fddb1b21033
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2020-02-25 21:37:14 +0200

    fix: change code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable (num) {
+ let a = ['zero','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+let b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+let res;
+let x = num%100;
+let y = num%10;
+	if(num<20) {
+		res = a[num];
+		}
+	
+	else if(num>19 && num<100) {
+		res = b[Math.trunc(num/10)] + ((num%10!=0)?" " + a[num%10]:"");
+		}
+	else if(num>99) {
+		res = a[Math.trunc(num/100)] +" hundred" + ((x < 20)?((x == 0)?"":" " + a[num%100]):((y == 0)?" " + b[Math.trunc(x/10)]:" " + b[Math.trunc(x/10)] + " "+a[y]));
+				}
+  return res;
   
 }
